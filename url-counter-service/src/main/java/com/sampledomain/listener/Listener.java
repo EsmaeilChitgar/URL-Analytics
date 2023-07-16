2b837b8b668abfd912b3dcad35af4cd8d7cb71c4
Esmaeil Chitgar <84287711+EsmaeilChitgar@users.noreply.github.com>
2023-07-16 07:29:39 +0330

    Add the ability sending to the elasticsearch through the url-counter service

--- url-counter-service/src/main/java/com/sampledomain/listener/Listener.java
+++ url-counter-service/src/main/java/com/sampledomain/listener/Listener.java
@@ -1,6 +1,8 @@
 package com.sampledomain.listener;
 
+import com.sampledomain.elasticsearch.ElasticsearchResource;
 import com.sampledomain.messages.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,9 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 @KafkaListener(id = "listenerURLCounter", topics = "${message.topic1}")
 public class Listener {
+    @Autowired
+    private ElasticsearchResource elasticsearchResource;
+
     @KafkaHandler
     public void handleSenderMessage(Message message) {
         System.out.println("URLCounter listener: Message received: " + message.message());
+        elasticsearchResource.send(message);
     }
 
     @KafkaHandler(isDefault = true)

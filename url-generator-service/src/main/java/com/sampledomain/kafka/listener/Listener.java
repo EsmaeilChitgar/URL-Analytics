d3814db2f7f15d09411a4bb91defc55d1dcc1f6c
ec.chitgar <ec.chitgar@gmail.com>
2023-07-21 10:05:10 +0330

    add Slf4j logger

--- url-generator-service/src/main/java/com/sampledomain/kafka/listener/Listener.java
+++ url-generator-service/src/main/java/com/sampledomain/kafka/listener/Listener.java
@@ -1,5 +1,7 @@
 package com.sampledomain.kafka.listener;
 
+import static com.sampledomain.log.LogManager.LOG;
+
 import com.sampledomain.messages.Message;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,16 +12,16 @@ import org.springframework.stereotype.Component;
 public class Listener {
   @KafkaHandler
   public void handleSenderMessage(Message msg) {
-    System.out.println("URLGenerator listener: Message received: " + msg.message());
+    LOG.info("zzzzzzzzzzURLGenerator listener: Message received: " + msg.message());
   }
 
   @KafkaHandler
   public void handleSenderMessage(String msg) {
-    System.out.println("URLGenerator listener: String received: " + msg);
+    LOG.info("URLGenerator listener: String received: " + msg);
   }
 
   @KafkaHandler(isDefault = true)
   public void unknown(Object object) {
-    System.out.println("URLGenerator listener: Unknown type received: " + object);
+    LOG.error("URLGenerator listener: Unknown type received: " + object);
   }
 }

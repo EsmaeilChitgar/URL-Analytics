0b43b148ee2f450bf2c9afdc8e26863b83dfab8e
ec.chitgar <ec.chitgar@gmail.com>
2023-07-30 20:33:34 +0330

    apply google-formatter

--- url-generator-service/src/main/java/com/echitgar/URLGeneratorServiceApplication.java
+++ url-generator-service/src/main/java/com/echitgar/URLGeneratorServiceApplication.java
@@ -1,14 +1,8 @@
 package com.echitgar;
 
-// import static com.echitgar.common.log.LogManager.LOG;
-//
-// import com.echitgar.bootstrap.BootstrapService;
-// import jakarta.annotation.PostConstruct;
-// import org.springframework.beans.factory.annotation.Autowired;
 import com.echitgar.bootstrap.BootstrapService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
